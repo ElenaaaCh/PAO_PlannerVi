c14bdceaebf7e20865153e788bd0dc3cec71b5ae
ElenaaaCh <elena.chilese@studenti.unipd.it>
2023-09-05 16:47:05 +0200

    polimorfismo

--- Controller/admin_controller.cpp
+++ Controller/admin_controller.cpp
@@ -4,23 +4,36 @@ AdminController::AdminController(){}
 
 AdminController::AdminController(storage* s, admin_view * a, Controller* c) : Controller(s, a, c){
     view->setTitolo("VISUALIZZAZIONE ADMIN");
+    connect(view,SIGNAL(View_concerto_signal()),this,SLOT(concerto_enter()));
+    connect(view,SIGNAL(View_str_signal()),this,SLOT(str_enter()));
+    connect(view,SIGNAL(View_studio_signal()),this,SLOT(studio_enter()));
+}
 
-
-/*
+void AdminController::concerto_enter() const {
     Aula_View* aW_c = new aulaConcerto_view(QSize(400,500),view);
-    Aula_View* aW_s = new aulaStrumentale_view(QSize(400,500),view);
-    Aula_View* aW_st = new aulaStudio_view(QSize(400,500),view);
+    aula_controller* ac= new aula_controller (getModel(), aW_c, const_cast<AdminController*>(this));
+    ac->getView()->createTable();
 
-    aule_controller* miracolo= new aule_controller(s, a, aW_c, aW_s, aW_st, this);
-    miracolo->show();
-*/
-    /*
-    getView()->create_table({"Numero Aula","Data","Ora Arrivo", "Ora Uscita", "Causale", "Email utente","Rimuovi","Aggiungi"});
-    getView()->visualizza_prenotazioni(s->getContPren());
-    connect(view,SIGNAL(rimuovi_signal(uint)),this,SLOT(rimuovi_enter(uint)));
-    connect(view,SIGNAL(rimuovi_signal(uint)),this,SLOT(rimuovi_enter(uint)));
-    connect(view,SIGNAL(aggiugi_signal(const int, const QDate, const QTime, const QTime, const QString, const QString)),this,SLOT(aggiungi_enter(const int, const QDate, const QTime, const QTime, const QString, const QString)));
-*/
+    contenitore<aula*> c = getModel()->getContAula1();
+    ac->getView()->carica_view(c);
+    hide();
+    ac->show();
+}
+
+void AdminController::str_enter() const {
+    Aula_View* aW_s = new aulaStrumentale_view(QSize(400,500),view);
+    aula_controller* ac= new aula_controller (getModel(), aW_s, const_cast<AdminController*>(this));
+    ac->getView()->createTable();
+    hide();
+    ac->show();
+}
+
+void AdminController::studio_enter() const {
+    Aula_View* aW_st = new aulaStudio_view(QSize(400,500),view);
+    aula_controller* ac= new aula_controller (getModel(), aW_st, const_cast<AdminController*>(this));
+    ac->getView()->createTable();
+    hide();
+    ac->show();
 }
 
 
