c5cb5529b757e840af22687069f63c25e156af91
marcobrugin <marcobrugin00@gmail.com>
2023-09-07 11:13:32 +0200

    Fix segnali

--- View/aula_view.h
+++ View/aula_view.h
@@ -23,7 +23,12 @@ public:
    virtual  void rimuovi_aula(uint)=0;
 
 signals:
-    void viewClosed() const; //close_signal to controller
+    void rimuovi_signal_studio(uint) const;
+    void aggiungi_signal_s(const int&, const int&, const QString&, const int&, const int&, const int&);
+    void aggiungi_signal_c(const int&, const int&, const QString&, const int&, const QString&,const QString&, const int&, const bool&);
+    void rimuovi_signal_concerto(uint) const;
+    void rimuovi_signal_strumentale(uint) const;
+    void aggiungi_signal_str(const int&, const int&, const QString&, const int&, const QString&);
 };
 
 #endif // AULA_VIEW_H
